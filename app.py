87e7000ba44d9aba6b62309796616f9ca77cec4a
dsarthak0 <131682623+dsarthak0@users.noreply.github.com>
2025-01-13 23:47:09 +0530

    Initial commit

--- app.py
+++ app.py
@@ -1,50 +1,70 @@
 import streamlit as st
 import pickle
 import pandas as pd
-import requests
-def fetch_poster(movie_id):
-    response=requests.get('https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US'.format(movie_id))
-    data=response.json()
-    return "http://image.tmdb.org/t/p/w500/"+data['poster_path']
+import os
+
+# Load environment variables from the .env file
+# (Make sure the dotenv and API key part is correct for your use case, but it's not used in this version)
+# load_dotenv()  # Load environment variables
+# api_key = os.getenv("TMDB_API_KEY")  # Fetch the API key from .env
+
+# Set the TMDb API endpoint
+TMDB_HOST = "themoviedb.p.rapidapi.com"
+
+
+@st.cache_data
+def load_data():
+    """Load movie data and similarity matrix from pickle files."""
+    try:
+        movies_dict = pickle.load(open('movie_dict.pkl', 'rb'))
+        movies = pd.DataFrame(movies_dict)
+        similarity = pickle.load(open('similarity.pkl', 'rb'))
+        return movies, similarity
+    except FileNotFoundError as e:
+        st.error("Required data files are missing. Please ensure `movie_dict.pkl` and `similarity.pkl` are available.")
+        st.stop()
+
 
 def recommend(movie):
-    movie_index=movies[movies['title']==movie].index[0]
-    distances=similarity[movie_index]
-    movies_list=sorted(list(enumerate(distances)),reverse=True,key=lambda x:x[1])[1:6]
+    """Recommend movies based on similarity matrix."""
+    try:
+        movie_index = movies[movies['title'] == movie].index[0]
+        distances = similarity[movie_index]
+        movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
 
-    recommended_movies=[]
-    recommended_movie_posters=[]
-    for i in movies_list:
-        movie_id=movies.iloc[i[0]].movie_id
+        recommended_movies = []
+        for i in movies_list:
+            recommended_movies.append(movies.iloc[i[0]].title)
 
-        recommended_movies.append(movies.iloc[i[0]].title)
-        recommended_movie_posters.append(fetch_poster(movie_id))
-    return recommended_movies
-movies_dict=pickle.load(open('movie_dict.pkl', 'rb'))
-movies=pd.DataFrame(movies_dict)
-similarity=pickle.load(open('similarity.pkl', 'rb'))
-st.title('Movie Recommendation system')
-selected_movie_name=st.selectbox(
-    'How would you like to be contacted?',movies['title'].values
-)
+        return recommended_movies
+    except IndexError:
+        st.error(f"Could not find recommendations for the movie: {movie}")
+        return []
+
+
+# Load movie data and similarity matrix
+movies, similarity = load_data()
+
+# Streamlit App
+st.title('Movie Recommendation System')
+
+# Select a movie from the list
+selected_movie_name = st.selectbox('Select a movie:', movies['title'].values)
+
+# When the "Recommend" button is pressed
 if st.button('Recommend'):
-    names,poster=recommend(selected_movie_name)
-    col1,col2,col3,col4,col5=st.beta_columns(5)
-    with col1:
-        st.text(names[0])
-        st.image(poster[0])
-    with col2:
-        st.text(names[0])
-        st.image(poster[0])
-    with col3:
-        st.text(names[0])
-        st.image(poster[0])
-    with col4:
-        st.text(names[0])
-        st.image(poster[0])
-    with col5:
-        st.text(names[0])
-        st.image(poster[0])
+    names = recommend(selected_movie_name)
+
+    if names:
+        # Display recommended movies
+        st.write("Recommended Movies:")
+        for name in names:
+            st.text(name)
+    else:
+        st.error("No recommendations available.")
+
+
+
 
 
 
